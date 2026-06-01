bcdf9de4b4263620463b48ea8e07267c37c04340
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-06-01 18:22:33 +0200

    Register DbContext

--- APBD09/Program.cs
+++ APBD09/Program.cs
@@ -1,6 +1,12 @@
+using APBD09.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<UniversityTasksDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
